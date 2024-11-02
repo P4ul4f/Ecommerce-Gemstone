25f96abc8be5e501b7d4e8525d262a74ad00c980
Paula Ferreyra <paulaferreyra24@gmail.com>
2024-11-02 20:52:27 -0300

    cors fix

--- api/index.js
+++ api/index.js
@@ -33,13 +33,7 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: "*", // Permitir todos los orígenes
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true 
 };
