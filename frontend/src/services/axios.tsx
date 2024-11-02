a8b19be031ba64c9b542f062991a7cf5b4957227
Paula Ferreyra <paulaferreyra24@gmail.com>
2024-11-02 20:44:55 -0300

    base url fix

--- frontend/src/services/axios.tsx
+++ frontend/src/services/axios.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecommerce-gemstone-p4ul4fs-projects.vercel.app/api", 
+  baseURL: "https://ecommerce-gemstone-xd1v-2s7fx3uqf-p4ul4fs-projects.vercel.app", 
 });
 
 export default api;
